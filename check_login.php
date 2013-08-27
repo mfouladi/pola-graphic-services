6019db1bd587f4f33ca22afa9087375ce4a4bfd6
mfouladi <fouladim@gmail.com>
2013-08-27 14:18:36 -0700

    php connects to pola server with valid query

--- check_login.php
+++ check_login.php
@@ -1,23 +1,47 @@
 
 <?php
-//phpinfo();
-$db_host="POLA-SQLTEST"; // Host name
-$db_user="graphics"; // Mysql username
-$db_pwd="Graphics@service"; // Mysql password
-$db_name="Graphics_Services"; // Database name
-$tbl_name="users"; // Table name
+	//phpinfo();
+	ini_set('display_errors', 'On'); 
+	error_reporting(E_ALL);
+	$db_host="POLA-SQLTEST"; // Host name
+	$db_user="graphics"; // Mysql username
+	$db_pwd="Graphics@service"; // Mysql password
+	$db_name="Graphics_Services"; // Database name
+	$tbl_name="users"; // Table name
 
-try {  
-  # MS SQL Server with PDO_DBLIB  
-  $DBH = new PDO("sqlsrv:host=$db_host;dbname=$db_name", $db_user, $db_pwd, 80);  
-  echo "<p>BITCHES</p>";
-}  
-catch(PDOException $e) {  
-    echo $e->getMessage();
-echo "<p>cock suckers...</p>"; 	
-} 
+	//Connect
+	$connection = odbc_connect("Driver={SQL Server};Server=$db_host;Database=$db_name",$db_user,$db_pwd );
+	if($connection)
+		echo "<p>Successfully Connect</p>";
+	else
+		echo "<p>Failed to Connect</p>";
+	$firstName=$_POST['firstName'];
+	$lastName=$_POST['lastName']; 
+		
+	$sql = "SELECT first_name FROM users WHERE first_name='$firstName' and last_name='$lastName'";
+	$res = odbc_prepare($connection, $sql);
+	$success = odbc_execute($res);
+	if($success)
+		echo "<p>Successful Query</p>";
+	else
+		echo "<p>Failed to Query</p>";
+	$count = odbc_num_rows($res);
+	echo $count;
+	
+	
+	
+	
+	// username and password sent from form
+	/*$firstName=$_POST['firstName'];
+	$lastName=$_POST['lastName']; 
+	echo "<p>Successfully Received Data</p>";
+	$tsql = "SELECT first_name FROM users";
+	echo "<p> $tsql </p>";
+	//$connection->query($tsql);
 
-//Connect
+	echo "<p>Successfully Got Count</p>";*/
+
+
 //$mysqli = new mysqli($db_host, $db_user, $db_pwd, $db_name);
 //$result=mysql_query($sql);
 //if ($mysqli->connect_errno) {
