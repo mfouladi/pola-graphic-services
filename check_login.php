3034641b58f23c7ba18b7c72b4ae177214fb4e4d
mfouladi <fouladim@gmail.com>
2013-08-27 17:04:40 -0700

    Updated login, added index, and updated file system

--- check_login.php
+++ check_login.php
@@ -1,6 +1,5 @@
 
 <?php
-	//phpinfo();
 	ini_set('display_errors', 'On'); 
 	error_reporting(E_ALL);
 	$db_host="POLA-SQLTEST"; // Host name
@@ -11,48 +10,41 @@
 
 	//Connect
 	$connection = odbc_connect("Driver={SQL Server};Server=$db_host;Database=$db_name",$db_user,$db_pwd );
-	if($connection)
-		echo "<p>Successfully Connect</p>";
-	else
-		echo "<p>Failed to Connect</p>";
-	$firstName=$_POST['firstName'];
-	$lastName=$_POST['lastName']; 
+	
+	$firstName=$_POST['first-name'];
+	$lastName=$_POST['last-name'];
+	$requestType=$_POST['request-type'];
 		
-	$sql = "SELECT first_name FROM users WHERE first_name='$firstName' and last_name='$lastName'";
+	$sql = "SELECT * FROM users WHERE first_name='$firstName' and last_name='$lastName'";
 	$res = odbc_prepare($connection, $sql);
 	$success = odbc_execute($res);
-	if($success)
-		echo "<p>Successful Query</p>";
-	else
-		echo "<p>Failed to Query</p>";
+
 	$count = odbc_num_rows($res);
-	echo $count;
 	
-	
-	
-	
-	// username and password sent from form
-	/*$firstName=$_POST['firstName'];
-	$lastName=$_POST['lastName']; 
-	echo "<p>Successfully Received Data</p>";
-	$tsql = "SELECT first_name FROM users";
-	echo "<p> $tsql </p>";
-	//$connection->query($tsql);
+	session_start(); 
+	$_SESSION['firstName'] = $firstName;
+	$_SESSION['lastName'] = $lastName;
+	$_SESSION['requestType'] = $requestType;
+	$host  = $_SERVER['HTTP_HOST'];
+	$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+	if($count === 1){
+		$_SESSION['division'] = odbc_result($res, "division");
+		$_SESSION['phone_number'] = odbc_result($res, "phone_number");
+		$_SESSION['email'] = odbc_result($res, "email");
+		if($requestType === "Copy Center Request"){
+			$extra = 'copy_center.php';
+		}else if($requestType === "Design Request"){
+			$extra = 'design_request.php';
+		}else if($requestType === "Audio/Video Request"){
+			$extra = 'av_request.php';
+		}else{
+		
+		}
+		//$extra = 'main_login.php';
+	}else{
+		//$extra = 'main_login.php';
+	}
+	header("Location: http://$host$uri/$extra");	
 
-	echo "<p>Successfully Got Count</p>";*/
-
-
-//$mysqli = new mysqli($db_host, $db_user, $db_pwd, $db_name);
-//$result=mysql_query($sql);
-//if ($mysqli->connect_errno) {
- //   echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-//}
-//echo $mysqli->host_info . "\n";
-
-
-//$sql="SELECT * FROM $tbl_name WHERE first_name='$myusername' and last_name='$mypassword'";
-
-// Mysql_num_row is counting table row
-//$count=mysql_num_rows($result);
 
 ?>
---
+++ design_request.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="copy_center.css"/>
+        <title></title>
+    </head>
+    <body>
+        <div class="wrapper">
+			<h1>Design Request Form</h1>
+        </div>
+    </body>
+</html>
